5857d5eda56e31711c5d979a2ea2db8630fce7a7
{{name}} <{{desk_login}}@desk.com>
2016-06-30 10:56:48 -0400

    add inline template and style to StockNav component

--- src/compchapter/components/StockNav.ts
+++ src/compchapter/components/StockNav.ts
@@ -1,9 +1,22 @@
 import {Component} from 'angular2/core';
 
 @Component({
-  selector: 'StockNav',
-  templateUrl: './components/stockNav.html',
-  styles: ['div {background: yellow;}']
-  // styleUrls: ['./components/stockNav.css']
+  selector: 'StockNav'
+  template: `
+  <div class="row">
+    <div class="col-md-12">
+      <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+             <span class="sr-only">Toggle navigation</span>
+             <span class="glyphicon glyphicon-minus"></span>
+          </button>
+          <a class="navbar-brand" href="#">Branding (inline template)</a>
+        </div>
+      </nav>
+    </div>
+  </div>`,
+  styles: ['div {background: yellow;}', '.navbar-brand {font-size: 20px;}'],
+
 })
 export class StockNav {}
